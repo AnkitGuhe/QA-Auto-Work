9ddd4b072de19b97e91ae68422a26ef7d9c117a1
aguhe <aguhe@OAK-DESKTOP.CCI.local>
2019-05-23 14:41:20 -0400

    5/23/2019
    Screenshots added
    screenshot file names change with time
    parameterized the customer data using Array
    and ran a loop to try 4 customer information at a time

--- PractTest/src/TestNG/NewCustomer.java
+++ PractTest/src/TestNG/NewCustomer.java
@@ -22,11 +22,19 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class NewCustomer {
 WebDriver NewCustDriver;
+String Address[]= {"713 Activa","2969 kingsway","1565 windsor","1208 Kingsway"};
 
-  public NewCustomer(WebDriver NewCustDriver1)
+String Name[]= {"Ankit", "Ash", "shivani", "Akshay"};
+String DateOB[]= {"12211993", "12241993", "04211994", "07091993*)"};
+String City[]= {"Kitchener", "windsor", "waterloo", "cambridge"};
+String PIN[]= {"123456", "234567", "234243", "675665"};
+String Mob[]= {"2266069229", "2265088082", "2265813004", "2267490768"};
+String Email[]= {"A1.guhe22@gmail.com", "A28082@gmail.com", "s121194@gmail.com", "al0709@gmail.com"};
+String Pass[]= {"Ankit", "Ash", "shivani", "Akshay"};
+
+public NewCustomer(WebDriver NewCustDriver1)
 
 	{
 	this.NewCustDriver= NewCustDriver1;
@@ -34,16 +42,39 @@ WebDriver NewCustDriver;
 
 	}
   
-  @FindBy(xpath="/html/body/div[3]/div/ul/li[2]/a")
-  WebElement NewCustomer;
-
-  @FindBy(name="name")
-  WebElement NewCustmerName;
+	  @FindBy(xpath="/html/body/div[3]/div/ul/li[2]/a")
+	  WebElement NewCustomer;
+	
+	  @FindBy(name="name")
+	  WebElement NewCustmerName;
   
-@FindBy(xpath="/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[2]")
-WebElement RadBtnGender;
-  
-  
+	@FindBy(xpath="/html/body/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[2]")
+	WebElement RadBtnGender;
+	
+	@FindBy(id="dob")
+	WebElement DOB;
+	@FindBy(name= "addr")
+	WebElement Addr;
+	@FindBy(name="city")
+	WebElement city;
+	@FindBy(name="pinno")
+	WebElement pinno;
+	
+	@FindBy(name="telephoneno")
+	WebElement telephoneno;
+	
+	@FindBy(name="emailid")
+	WebElement emailid;  
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(name= "state")
+    WebElement State; 
+@FindBy(name= "sub")
+WebElement SubmitBtn;
+	
+	
   public WebElement NewCustomer()
 {
 return NewCustomer;
@@ -54,24 +85,60 @@ return NewCustomer;
   return RadBtnGender;
   }
  
-  public static void main(String []args)
+  public WebElement DOB()
   {
-try
-  {
-	 File New= new File("C:\\Users\\aguhe\\Desktop.a.xlsx");  
-	 FileInputStream fis=new FileInputStream(New);
-	 XSSFWorkbook wb=new XSSFWorkbook(fis);
-	   XSSFSheet sh1= wb.getSheetAt(0);
-	   System.out.println(sh1.getRow(0).getCell(2).getStringCellValue());
-
+	  return DOB;
   }
-catch (Exception e) {
-	 
-	   System.out.println(e.getMessage());
-	 
-	  }
-  } 
   
-  
+public WebElement Addr()
+{
+	
+return Addr;	
+}
+public WebElement city()
+{
+	
+return city;	
+}
+
+public WebElement pinno()
+{
+	
+return pinno;	
+}
+
+public WebElement telephoneno()
+{
+	
+return telephoneno;	
+}
+
+public WebElement password()
+{
+	
+return emailid;	
+}
+
+public WebElement emailid()
+{
+	
+return password;	
+}
+
+public WebElement State()
+{
+	
+return State;	
+}
+
+public WebElement SubBtn()
+{
+	
+return SubmitBtn;	
+}
+
+
+
+
 }
 
--- PractTest/src/TestNG/NewTest.java
+++ PractTest/src/TestNG/NewTest.java
@@ -1,6 +1,19 @@
 package TestNG;
 import org.testng.Assert;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -9,12 +22,14 @@ import org.testng.annotations.Test;
 
 
 public class NewTest {
+	
+	
 	public WebDriver driver ;
 String URL= "http://www.demo.guru99.com/V4/";
 String driverpath =	("C:\\chromedriver_win32\\chromedriver.exe");
 String userName= "mngr195228";
 String Password= "YhYhUhU";
-
+String scfile[]= {"1","2","3","4,","5"};
 @BeforeTest
  public void LaunchBrowser()
  {
@@ -22,8 +37,8 @@ String Password= "YhYhUhU";
 	driver = new ChromeDriver();
  driver.get(URL);
  System.out.println("launching Chrome Browser");
- }
  
+ }
  
  @Test(priority=0)
   public void VerifyPageTitle() 
@@ -48,15 +63,44 @@ String Password= "YhYhUhU";
  }
 
  @Test(priority=2)
+
  public void NewCustomer()
  {
-	 NewCustomer Ncust= new NewCustomer(driver);
-Ncust.NewCustomer.click();
- Ncust.NewCustmerName.sendKeys("Ankit Guhe");
- Ncust.RadBtnGender().click();
+ 
+ NewCustomer Ncust= new NewCustomer(driver);
+ Ncust.NewCustomer.click();
+ driver.manage().window().maximize();
+ 
+for (int i=0;i<=4;i++)
+{
+	 Ncust.NewCustmerName.sendKeys(Ncust.Name[i]);
+	 Ncust.DOB.sendKeys(Ncust.DateOB[i]); 
+	 Ncust.Addr.sendKeys(Ncust.Address[i]);
+	 Ncust.RadBtnGender().click();
+	 Ncust.pinno.sendKeys(Ncust.PIN[i]);
+	 Ncust.emailid.sendKeys(Ncust.Email[i]);
+	 Ncust.city.sendKeys(Ncust.City[i])	;
+	 Ncust.password.sendKeys(Ncust.Pass[i]);
+	 Ncust.telephoneno.sendKeys(Ncust.Mob[i]);
+	 Ncust.State.sendKeys("Ontario");
+	 Ncust.SubmitBtn.click();
+	
+	// Now you can do whatever you need to do with it, for example copy somewhere
+	 File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	 try {
+		 String fileName = new SimpleDateFormat("yyyyMMddHHmmss'.png'").format(new Date());
+ FileUtils.copyFile(screenshotFile, new File("A:\\TestData\\"+fileName));
+	
+	 } 
+	 catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 Ncust.NewCustomer.click();
  }
- 
- 
+
  
 }
+}
+
 
